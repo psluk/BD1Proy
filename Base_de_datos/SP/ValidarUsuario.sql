d9b65a9b36736031c09a305b15afd29216f5e111
psluk <p7285@outlook.com>
2022-10-13 20:18:42 -0600

    Add login check SP #6

---
+++ Base_de_datos/SP/ValidarUsuario.sql
@@ -0,0 +1,27 @@
+/*
+    Procedimiento que verifica un nombre de usuario y una contraseña
+    con los datos presentes en la base de datos.
+
+    El nombre de usuario no distingue entre mayúsculas y minúsculas,
+    pero la contraseña sí
+*/
+
+ALTER PROCEDURE [dbo].[ValidarUsuario]
+    -- Se definen las variables de entrada
+    @inUsername VARCHAR(32),
+    @inClave VARCHAR(32)
+AS
+BEGIN
+    SET NOCOUNT ON;         -- Para evitar interferencias
+
+    -- Retorna 1 si lo encuentra. Si no, 0
+    SELECT (CASE
+        WHEN EXISTS (SELECT 1 FROM [dbo].[Usuario] U
+            WHERE U.nombreDeUsuario = @inUsername
+                AND CAST(U.clave AS BINARY) = CAST(@inClave AS BINARY))
+        THEN 1
+        ELSE 0
+        END) AS 'Resultado'
+
+    SET NOCOUNT OFF;
+END;
